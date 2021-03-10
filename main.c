33d7d03d120117b312320996d39660a66ca6adbb
YunusHaas <m@lootboxcash.com>
2021-03-09 20:37:58 -0800

    clarity

--- main.c
+++ main.c
@@ -256,6 +256,7 @@ void _get_unique(uint8_t keypool[], int keypool_size, u64 gatekey, uint8_t uniqu
       first_layer += (second_layer % 2) ? 1 : -1;
     }
 
+    //This is a kaleidoscope effect - with layers of prng:
     //Get our first layer in place
     xor_bits(unique + current_pos, keypool, keypool_size, first_layer, chunk);
     //Add our random 2nd layer to make (noise ^ noise)
@@ -584,85 +585,6 @@ static void crng_reseed(struct crng_state *crng, struct entropy_store *r)
   primary_crng.init_time = jiffies;
 }
 
-
-/*
- * Credit (or debit) the entropy store with n bits of entropy.
- * Use credit_entropy_bits_safe() if the value comes from userspace
- * or otherwise should be checked for extreme values.
- */
-static void credit_entropy_bits(struct entropy_store *r, int nbits)
-{
-  int entropy_count, orig, has_initialized = 0;
-  const int pool_size = r->poolinfo->poolfracbits;
-  int nfrac = nbits << ENTROPY_SHIFT;
-
-  if (!nbits)
-    return;
-
-retry:
-  entropy_count = orig = READ_ONCE(r->entropy_count);
-  if (nfrac < 0) {
-    /* Debit */
-    entropy_count += nfrac;
-  } else {
-    /*
-     * Credit: we have to account for the possibility of
-     * overwriting already present entropy.  Even in the
-     * ideal case of pure Shannon entropy, new contributions
-     * approach the full value asymptotically:
-     *
-     * entropy <- entropy + (pool_size - entropy) *
-     *  (1 - exp(-add_entropy/pool_size))
-     *
-     * For add_entropy <= pool_size/2 then
-     * (1 - exp(-add_entropy/pool_size)) >=
-     *    (add_entropy/pool_size)*0.7869...
-     * so we can approximate the exponential with
-     * 3/4*add_entropy/pool_size and still be on the
-     * safe side by adding at most pool_size/2 at a time.
-     *
-     * The use of pool_size-2 in the while statement is to
-     * prevent rounding artifacts from making the loop
-     * arbitrarily long; this limits the loop to log2(pool_size)*2
-     * turns no matter how large nbits is.
-     */
-    int pnfrac = nfrac;
-    const int s = r->poolinfo->poolbitshift + ENTROPY_SHIFT + 2;
-    /* The +2 corresponds to the /4 in the denominator */
-
-    do {
-      unsigned int anfrac = min(pnfrac, pool_size/2);
-      unsigned int add =
-        ((pool_size - entropy_count)*anfrac*3) >> s;
-
-      entropy_count += add;
-      pnfrac -= anfrac;
-    } while (unlikely(entropy_count < pool_size-2 && pnfrac));
-  }
-
-  if (unlikely(entropy_count < 0)) {
-    pr_warn("random: negative entropy/overflow: pool %s count %d\n",
-      r->name, entropy_count);
-    WARN_ON(1);
-    entropy_count = 0;
-  } else if (entropy_count > pool_size)
-    entropy_count = pool_size;
-  if ( !r->initialized &&
-      (entropy_count >> ENTROPY_SHIFT) > 128)
-    has_initialized = 1;
-  if (cmpxchg(&r->entropy_count, orig, entropy_count) != orig)
-    goto retry;
-
-  if (has_initialized) {
-    r->initialized = 1;
-    //wake_up_interruptible(&random_read_wait);
-    //kill_fasync(&fasync, SIGIO, POLL_IN);
-  }
-
-  trace_credit_entropy_bits(r->name, nbits,
-          entropy_count >> ENTROPY_SHIFT, _RET_IP_);
-}
-
 /*
  * Getting entropy on a fresh system is a hard thing to do. 
  * So, we will start with latent_entropy, although it isn't required it doesn't hurt.
@@ -726,7 +648,7 @@ static void find_more_entropy_in_memory(struct crng_state *crng, int nbytes_need
   _add_unique(crng, POOL_SIZE, gatekey, anvil, nbytes_needed, nbytes_needed);
   
   //Clean up our tracks so another process cannot see our source material
-  memset(anvil, 0, nbytes_needed);
+  memzero_explicit(anvil, nbytes_needed);
   gatekey = 0;
   free(anvil);
 }
