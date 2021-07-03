94ccc2e1b3ea9ded9b39a758b0dc09ba1625b7fa
m <firealwaysworks@gmail.com>
2021-07-02 21:58:37 -0700

    adding primes

--- knockout.h
+++ knockout.h
@@ -110,6 +110,14 @@ static const struct poolinfo {
 #endif
 };
 
+//All primes less than 1024
+const int keypool_primes[] = {3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127};//, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021};
+
+
+#ifndef __min
+   #define __min( x, y )  (((x) < (y))?(x):(y))
+#endif
+
 //#define __printf void(1, 2);
 //https://elixir.bootlin.com/linux/latest/source/arch/powerpc/boot/types.h#L14
 typedef unsigned char		u8;
@@ -125,7 +133,7 @@ typedef long long		s64;
 typedef u8  uint8_t;
 typedef u16 uint16_t;
 typedef u32 uint32_t;
-typedef u64 uint64_t;
+//typedef u64 uint64_t;
 typedef s8  int8_t;
 typedef s16 int16_t;
 typedef s32 int32_t;
@@ -193,4 +201,3 @@ static long random_get_entropy(){
 }
 
 static struct crng_state primary_crng;
-
